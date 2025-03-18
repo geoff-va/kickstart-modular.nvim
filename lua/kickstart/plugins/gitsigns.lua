005730cd554263aff43fcabdb8435f597582c169
Geoff Von Allmen <geoff@ibleducation.com>
2025-03-18 10:48:34 -0700

    refactor: Add git blame to gitsigns

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -24,6 +24,14 @@ return {
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+			current_line_blame = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 1000,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+			},
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
