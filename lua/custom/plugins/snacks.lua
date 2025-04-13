2aaf9af8494016f7fc4f0793239e3d473806c6a9
Geoff Von Allmen <geoff@ibleducation.com>
2025-04-13 13:32:34 -0700

    refactor: re-enable toggles

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -460,15 +460,14 @@ return {
         end
         vim.print = _G.dd -- Override print to use snacks for `:=` command
 
-        -- These curently throw which-key errors
         -- Create some toggle mappings
         Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
-        -- Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
-        -- Snacks.toggle.diagnostics():map '<leader>ud'
+        Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
+        Snacks.toggle.diagnostics():map '<leader>ud'
         -- Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>uc'
-        -- Snacks.toggle.treesitter():map '<leader>uT'
+        Snacks.toggle.treesitter():map '<leader>uT'
         -- Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
-        -- Snacks.toggle.inlay_hints():map '<leader>uh'
+        Snacks.toggle.inlay_hints():map '<leader>uh'
         -- Snacks.toggle.indent():map '<leader>ug'
         -- Snacks.toggle.dim():map '<leader>uD'
       end,
