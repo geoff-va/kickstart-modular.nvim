f0ae9006bbfb7725f1b3eea58c2d919c58d7432b
Geoff Von Allmen <geoff@ibleducation.com>
2025-04-23 10:14:56 -0700

    refactor: fixup tmux-pytest plugin

--- lua/custom/plugins/nvim-tmux-pytest.lua
+++ lua/custom/plugins/nvim-tmux-pytest.lua
@@ -1,11 +1,13 @@
 return {
-	dir = "/Users/geoff/Documents/projects/nvim-tmux-pytest/",
-	opts = {
-		base_cmd = "pytest --disable-warnings --no-migrations",
-	},
-	keys = {
-		{ "<leader>tf", "<cmd>TPRunFunction<cr>", desc = "TP: Run function/method" },
-		{ "<leader>tc", "<cmd>TPRunClass<cr>", desc = "TP: Run Class" },
-		{ "<leader>tw", "<cmd>TPSelectWindow<cr>", desc = "TP: Select target window" },
-	},
+  dir = '/Users/geoff/Documents/projects/nvim-tmux-pytest/',
+  opts = {
+    base_cmd = 'pytest --disable-warnings --no-migrations',
+  },
+  keys = {
+    { '<leader>tf', '<cmd>TPRunFunction<cr>', desc = 'TP: Run current test' },
+    { '<leader>tF', '<cmd>TPRunFile<cr>', desc = 'TP: Run tests for file' },
+    { '<leader>tc', '<cmd>TPRunClass<cr>', desc = 'TP: Run current class tests' },
+    { '<leader>tb', '<cmd>TPSetBaseCommand<cr>', desc = 'TP: Set base test command' },
+    { '<leader>tw', '<cmd>TPSelectWindow<cr>', desc = 'TP: Select target window' },
+  },
 }
